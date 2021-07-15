5df1d532cf8784e74b6046720d36727332c04d32
a.scherbatsky <a.scherbatsky@asferro.com>
2021-07-15 09:57:26 +0300

    Refactoring

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -5,19 +5,15 @@ const chalk = require(`chalk`);
 const {DEFAULT_PORT} = require(`../../constants`);
 const apiRouter = require(`../api`);
 
-const app = express();
-app.use(express.json());
-
-(async () => {
-  await apiRouter(app);
-})();
-
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = +customPort || DEFAULT_PORT;
+    const app = express();
 
+    app.use(express.json());
+    await apiRouter(app);
     app.listen(port, () => console.info(chalk.green(`Waiting for connection on ${port}`)));
     app.on(`error`, ({message}) => console.error(`Server creation error: ${message}`));
   },
