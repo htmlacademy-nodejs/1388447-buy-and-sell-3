c4a03510c1f64ddc2a5af8647fe09c38f3672875
a.scherbatsky <a.scherbatsky@asferro.com>
2021-07-16 11:20:06 +0300

    Added logger Pino

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const express = require(`express`);
-const chalk = require(`chalk`);
 const {DEFAULT_PORT} = require(`../../constants`);
 const apiRouter = require(`../api`);
+const {getLogger} = require(`../lib/logger`);
+
+const logger = getLogger({name: `api`});
 
 module.exports = {
   name: `--server`,
@@ -12,9 +14,18 @@ module.exports = {
     const port = +customPort || DEFAULT_PORT;
     const app = express();
 
-    app.use(express.json());
-    await apiRouter(app);
-    app.listen(port, () => console.info(chalk.green(`Waiting for connection on ${port}`)));
-    app.on(`error`, ({message}) => console.error(`Server creation error: ${message}`));
+    try {
+      app.use(express.json());
+      await apiRouter(app);
+
+      app.listen(port, (err) => {
+        if (err) {
+          return logger.error(`An error occurred on server creation: ${err.message}`);
+        }
+        return logger.info(`Listening to connections on ${port}`);
+      });
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+    }
   },
 };
