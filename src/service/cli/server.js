0bb0c04a03003180aeb56094a470b2d696aa1de5
a.scherbatsky <a.scherbatsky@asferro.com>
2021-07-19 17:27:27 +0300

    Added logger middlewares

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,22 +1,35 @@
 'use strict';
 
 const express = require(`express`);
-const {DEFAULT_PORT} = require(`../../constants`);
+const {DEFAULT_PORT, HttpCode} = require(`../../constants`);
 const apiRouter = require(`../api`);
 const {getLogger} = require(`../lib/logger`);
-
-const logger = getLogger({name: `api`});
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = +customPort || DEFAULT_PORT;
+    const logger = getLogger({name: `api`});
     const app = express();
 
     try {
       app.use(express.json());
+      app.use((req, res, next) => {
+        logger.debug(`Request on route ${req.url}`);
+        res.on(`finish`, () => {
+          logger.info(`Response status code ${res.statusCode}`);
+        });
+        next();
+      });
       await apiRouter(app);
+      app.use((err, _req, _res, _next) => {
+        logger.error(`An error occurred on processing request: ${err.message}`);
+      });
+      app.use((req, res) => {
+        res.status(HttpCode.NOT_FOUND);
+        logger.error(`Route not found: ${req.url}`);
+      });
 
       app.listen(port, (err) => {
         if (err) {
